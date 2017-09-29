83d52a707375d8be3bb58afcaece299d909f9e1a
Luka Pevec <luka.pevec@fer.hr>
2017-09-30 00:43:46 +0200

    Small comment add

--- src/Exp.cpp
+++ src/Exp.cpp
@@ -7,9 +7,12 @@ Exp::Exp(const DerivableFunction *argument) : argument_(argument) {}
 string Exp::ToString() const {
   return "exp(" + argument_->ToString() + ")";
 }
+
+// (e(x))' = e(x) * x'
 DerivableFunction *Exp::Derive() const {
   return *this * argument_->Derive();
 }
+
 double Exp::Evaluate(double d) const {
   return exp(d);
 }
